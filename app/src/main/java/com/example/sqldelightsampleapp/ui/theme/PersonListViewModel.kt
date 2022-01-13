669f873d6e64b6492c83221bc9f9c433819afd18
Akshata-Ingenie <92729673+Akshata-Ingenie@users.noreply.github.com>
2022-01-13 11:42:15 +0000

    Implemented viewmodel for inserting, deleting, showing person person by id and handling first name and last name text changes

---
+++ app/src/main/java/com/example/sqldelightsampleapp/ui/theme/PersonListViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.sqldelightsampleapp.ui.theme
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sqldelightsampleapp.data.PersonDataSource
+import kotlinx.coroutines.launch
+import sampleapp.persondb.PersonEntity
+import javax.inject.Inject
+
+class PersonListViewModel @Inject constructor(private val personDataSource: PersonDataSource) :
+    ViewModel() {
+
+    val persons = personDataSource.getAllPersons()
+
+    // we by using private set only this viewmodel can change its value
+    var personDetails by mutableStateOf<PersonEntity?>(null)
+        private set
+
+    var firstNameText by mutableStateOf("")
+        private set
+
+    var lastNameText by mutableStateOf("")
+        private set
+
+    fun onInsertPersonClick() {
+        if (firstNameText.isBlank() || lastNameText.isBlank()) {
+            return
+        }
+
+        viewModelScope.launch {
+            personDataSource.insertPerson(firstName = firstNameText, lastName = lastNameText)
+            firstNameText = ""
+            lastNameText = ""
+        }
+    }
+
+    fun onDeleteClick(id: Long) {
+        viewModelScope.launch {
+            personDataSource.deletePersonById(id)
+        }
+    }
+
+    fun getPersonById(id: Long) {
+        viewModelScope.launch {
+            personDetails = personDataSource.getPersonById(id)
+        }
+    }
+
+    fun onFirstNameChange(value: String) {
+        firstNameText = value
+    }
+
+    fun onLastNameChange(value: String) {
+        lastNameText = value
+    }
+}
